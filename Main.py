743ddba4cd1c99b3993026986b87199cb6c0f8a0
dinakaranaappu <110010672+dinakaranaappu@users.noreply.github.com>
2022-08-02 11:46:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,8 +5,7 @@ class Evaluate:
       size_of_stack: An integer which represents the size of stack.
       stack: A List which acts as a Stack.
   """
-    print(hello)
-
+    # Write your code here
 
   def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
@@ -14,9 +13,8 @@ class Evaluate:
       size_of_stack: An integer to set the size of stack.
     """
     self.top = -1
-    self.size_of_stack = size
-    self.stack = []
-
+    self.size = size
+    self.lst = [None]*size
 
   def isEmpty(self):
     """
@@ -24,8 +22,18 @@ class Evaluate:
     Returns:
       True if it is empty, else returns False.
     """
-      print(true)
-
+    # Write your code here
+    if self.top == -1:
+       return 1
+    else :
+       return 0
+          
+  def is_full(self):
+    # Write code here
+    if self.top == (self.size - 1):
+      return 1
+    else :
+      return 0
 
   def pop(self):
     """
@@ -33,8 +41,12 @@ class Evaluate:
     Returns:
       The data which is popped out if the stack is not empty.
     """
-    print(true)
-
+    # Write your code here
+    if not self.isEmpty():
+      t=self.lst[self.top]
+      del self.lst[self.top]
+      self.top-=1
+      return t
 
   def push(self, operand):
     """
@@ -42,8 +54,10 @@ class Evaluate:
     Arguments:
       operand: The operand to be pushed.
     """
-    print(true)
-
+    # Write your code here
+    if not self.is_full():
+            self.top+=1
+            self.lst[self.top]=operand
 
   def validate_postfix_expression(self, expression):
     """
@@ -53,8 +67,18 @@ class Evaluate:
     Returns:
       True if the expression is valid, else returns False.
     """
-    print(false)
-
+    # Write your code here
+    c1=0
+    c2=0
+    for i in expression:
+      if i.isdigit():
+        c1+=1
+      else:
+        c2+=1
+    if c1>c2 and expression[0].isdigit() and expression[1].isdigit():
+      return 1
+    else:
+      return 0
 
   def evaluate_postfix_expression(self, expression):
     """
@@ -64,8 +88,18 @@ class Evaluate:
     Returns:
       The result of evaluated postfix expression.
     """
-    print(false)
-
+    # Write your code here
+    for i in expression:
+      if i.isdigit():
+        self.push(i)
+      else:
+        v1 = self.pop()
+        v2 = self.pop()
+        if i=='/':
+          self.push(str(eval(v2 + i*2 + v1)))
+        else:
+          self.push(str(eval(v2 + i + v1)))
+    return self.pop()
 
 # Do not change the following code
 postfix_expression = input()  # Read postfix expression
